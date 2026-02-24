02f29d0a9f4b4042966146beb278124bf492d771
Mahmoud Nasser <mestek2001@gmail.com>
2026-02-24 04:20:34 +0200

    new commit

--- lib/core/widget/custom_button.dart
+++ lib/core/widget/custom_button.dart
@@ -1,7 +1,6 @@
 import 'package:bookia/core/app_themes/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
