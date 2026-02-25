7203f1e88959d6acaf39bf1afad3990166ecf2a2
Mahmoud <mestek2000@gmail.com>
2026-02-25 02:18:30 +0200

    new commit

---
+++ lib/core/localization/generated/locale_keys.g.dart
@@ -0,0 +1,82 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: constant_identifier_names
+
+abstract class  LocaleKeys {
+  static const app_name = 'app_name';
+  static const order_now = 'order_now';
+  static const welcome_back = 'welcome_back';
+  static const register_title = 'register_title';
+  static const forgot_password = 'forgot_password';
+  static const otp_verification = 'otp_verification';
+  static const create_new_password = 'create_new_password';
+  static const password_changed = 'password_changed';
+  static const success = 'success';
+  static const username = 'username';
+  static const email = 'email';
+  static const password = 'password';
+  static const confirm_password = 'confirm_password';
+  static const new_password = 'new_password';
+  static const current_password = 'current_password';
+  static const full_name = 'full_name';
+  static const address = 'address';
+  static const phone = 'phone';
+  static const governorate = 'governorate';
+  static const login = 'login';
+  static const register = 'register';
+  static const send_code = 'send_code';
+  static const verify = 'verify';
+  static const reset_password = 'reset_password';
+  static const add_to_cart = 'add_to_cart';
+  static const checkout = 'checkout';
+  static const submit_order = 'submit_order';
+  static const back_to_login = 'back_to_login';
+  static const back_to_home = 'back_to_home';
+  static const update_profile = 'update_profile';
+  static const update_password = 'update_password';
+  static const buy = 'buy';
+  static const best_seller = 'best_seller';
+  static const search_store = 'search_store';
+  static const my_cart = 'my_cart';
+  static const total = 'total';
+  static const wishlist_title = 'wishlist_title';
+  static const profile_title = 'profile_title';
+  static const my_orders = 'my_orders';
+  static const edit_profile = 'edit_profile';
+  static const contact_us = 'contact_us';
+  static const privacy_terms = 'privacy_terms';
+  static const place_order = 'place_order';
+  static const select_governorate = 'select_governorate';
+  static const order_number = 'order_number';
+  static const total_amount = 'total_amount';
+  static const dont_have_account = 'dont_have_account';
+  static const already_have_account = 'already_have_account';
+  static const remember_password = 'remember_password';
+  static const didnt_receive_code = 'didnt_receive_code';
+  static const resend = 'resend';
+  static const password_changed_success = 'password_changed_success';
+  static const order_success = 'order_success';
+  static const usernameIsRequired = 'usernameIsRequired';
+  static const userNameMustBe316Character = 'userNameMustBe316Character';
+  static const emailIsRequired = 'emailIsRequired';
+  static const enterValidEmail = 'enterValidEmail';
+  static const enterValidEmailOrPhoneNumber = 'enterValidEmailOrPhoneNumber';
+  static const phoneNumberIsRequired = 'phoneNumberIsRequired';
+  static const enterValidPhoneNumber = 'enterValidPhoneNumber';
+  static const codeIsRequired = 'codeIsRequired';
+  static const enterValidCode = 'enterValidCode';
+  static const passwordIsRequired = 'passwordIsRequired';
+  static const passwordMustBeAtLeast8Characters = 'passwordMustBeAtLeast8Characters';
+  static const passwordMustContainLetters = 'passwordMustContainLetters';
+  static const passwordMustContainNumbers = 'passwordMustContainNumbers';
+  static const passwordsDoNotMatch = 'passwordsDoNotMatch';
+  static const or = 'or';
+  static const sign_in_with_google = 'sign_in_with_google';
+  static const sign_in_with_apple = 'sign_in_with_apple';
+  static const login_now = 'login_now';
+  static const Enter_the_verification_code_we_just_sent_on_your_email_address = 'Enter_the_verification_code_we_just_sent_on_your_email_address';
+  static const Dont_worry_It_occurs_Please_enter_the_email_address_linked_with_your_account = 'Dont_worry_It_occurs_Please_enter_the_email_address_linked_with_your_account';
+  static const Your_new_password_must_be_unique_from_those_previously_used = 'Your_new_password_must_be_unique_from_those_previously_used';
+  static const Your_password_has_been_changed_successfully = 'Your_password_has_been_changed_successfully';
+
+}
